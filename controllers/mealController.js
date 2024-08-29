14514e9c4f1cffbf688f9e094a9c1988d4b26ece
Ian Graham <i4ng1999@gmail.com>
2024-08-29 02:00:59 +0100

    create meal model and implement CRUD functionality for controller functions

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -1,28 +1,57 @@
 const asyncHandler = require('express-async-handler');
 
+const Meal = require('../models/mealModel');
+
+// @desc    Get Meals
+// @route   GET /api/meals
 const getMeals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get meals' });
+  const meals = await Meal.find();
+  res.status(200).json(meals);
 });
 
+// @desc    Add Meal
+// @route   POST /api/meals
 const addMeal = asyncHandler(async (req, res) => {
-  const meal = req.body;
   if (!req.body.name) {
     res.status(400);
     throw new Error('Please add a name field');
   }
-  res.status(201).json({ message: `Add meal: ${meal.name}` });
+
+  const meal = await Meal.create({
+    name: req.body.name,
+  });
+  res.status(201).json(meal);
 });
 
+// @desc    Update Meal
+// @route   PUT /api/meals/:id
 const updateMeal = asyncHandler(async (req, res) => {
-  if (!req.body.name) {
+  const meal = await Meal.findById(req.params.id);
+
+  if (!meal) {
     res.status(400);
-    throw new Error('Please add a name field');
+    throw new Error('Meal not found');
   }
-  res.status(200).json({ message: `Update meal ${req.params.id}` });
+
+  const updatedMeal = await Meal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedMeal);
 });
 
+// @desc    Remove Meal
+// @route   DELETE /api/meals/:id
 const removeMeal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Remove meal ${req.params.id}` });
+  const meal = await Meal.findById(req.params.id);
+
+  if (!meal) {
+    res.status(400);
+    throw new Error('Meal not found');
+  }
+
+  await meal.deleteOne();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
